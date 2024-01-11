68d4f1acdf4fd7aa65d26d8c534dd14c437d77e5
Levi-24 <leventemajor36@gmail.com>
2024-01-11 10:26:48 +0100

    Horizontal movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float maxSpeed = 7;
+    
+    private Animator animator;
+    private Rigidbody2D rigidbody2d;
+
+    private bool isFacingRight;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        isFacingRight = true;
+    }
+
+    private void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(move));
+        rigidbody2d.velocity = new(
+            move * maxSpeed,
+            rigidbody2d.velocity.y
+        );
+
+        if ((move > 0 && !isFacingRight) || (move < 0 && isFacingRight)) Flip();
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.localScale = new(
+            transform.localScale.x * -1,
+            transform.localScale.y);
+    }
+}
