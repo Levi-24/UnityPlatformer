a9cb7ee4c836bbf7b7870c530d662cbae805079d
Levi-24 <leventemajor36@gmail.com>
2024-01-16 08:35:27 +0100

    Jump script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float maxSpeed = 7;
-    
+    [SerializeField] private float jumpHeight = 150;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
     private Animator animator;
     private Rigidbody2D rigidbody2d;
-
     private bool isFacingRight;
+    private bool isGrounded;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
         rigidbody2d = GetComponent<Rigidbody2D>();
         isFacingRight = true;
+        isGrounded = false;
+    }
+
+    private void Update()
+    {
+        if (isGrounded && Input.GetAxis("Jump") > 0)
+        {
+            isGrounded = false;
+            animator.SetBool("IsGrounded", false);
+            rigidbody2d.AddForce(new Vector2(0, jumpHeight));
+        }
     }
 
     private void FixedUpdate()
@@ -27,6 +42,10 @@ public class PlayerController : MonoBehaviour
         );
 
         if ((move > 0 && !isFacingRight) || (move < 0 && isFacingRight)) Flip();
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, .15f, groundLayer);
+        animator.SetBool("IsGrounded", isGrounded);
+        animator.SetFloat("VerticalSpeed", rigidbody2d.velocity.y);
     }
 
     private void Flip()
