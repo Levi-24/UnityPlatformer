501d0d612c6e9b89005d5f1835c2ca45523b83a2
Levi-24 <leventemajor36@gmail.com>
2024-02-13 12:00:34 +0100

    rockDamage

--- Assets/Scripts/Enemy Damage.cs
+++ Assets/Scripts/Enemy Damage.cs
@@ -4,7 +4,7 @@ public class EnemyDamage : MonoBehaviour
 {
     [SerializeField] private float damageRate = 1f;
     [SerializeField] private float damage = 2;
-    [SerializeField] private float pushForce;
+    [SerializeField] private float pushForce = 20f;
 
     private float nextDamage;
 
@@ -20,13 +20,14 @@ public class EnemyDamage : MonoBehaviour
             PlayerHealth playerHealth = player.gameObject.GetComponent<PlayerHealth>();
             playerHealth.TakeDamage(damage);
             nextDamage = Time.time + damageRate;
-
-
+            PushBack(player.transform);
         }
     }
 
-    private void PushBack(Transform pusher)
+    private void PushBack(Transform player)
     {
-
+        Rigidbody2D playerRb = player.gameObject.GetComponent<Rigidbody2D>();
+        playerRb.velocity = Vector2.zero;
+        playerRb.AddForce(new Vector2(0,1) * pushForce, ForceMode2D.Impulse);
     }
 }
