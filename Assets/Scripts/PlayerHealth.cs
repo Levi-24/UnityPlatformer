6424b67f9e9b29f1c364a1ff8c3f9de953746df9
Levi-24 <leventemajor36@gmail.com>
2024-03-05 08:29:42 +0100

    asd

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,9 +1,11 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private GameObject bloodDrops;
+    [SerializeField] private Slider healthBar;
     [SerializeField] private float maxHealth = 10f;
     private float currentHealth;
 
@@ -11,6 +13,8 @@ public class PlayerHealth : MonoBehaviour
 
     private void Start()
     {
+        healthBar.maxValue = maxHealth;
+        healthBar.value = maxHealth;
         currentHealth = maxHealth;
         //controller = GetComponent<PlayerController>();
     }
@@ -18,6 +22,7 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
+        healthBar.value -= damage;
         Instantiate(bloodDrops, transform.position, transform.rotation);
         if (currentHealth <= 0)
         {
